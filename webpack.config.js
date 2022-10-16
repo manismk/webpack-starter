133ae646d49aa7e8e00cff51c3ec4af9a7ff3a05
manismk <manismkmech@gmail.com>
2022-10-16 18:18:00 +0530

    feat: added Html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: { bundle: path.resolve(__dirname, "src/index.js") },
   output: { path: path.resolve(__dirname, "dist") },
+  mode: "production",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/template.html",
+    }),
+  ],
 };
