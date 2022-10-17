671b9a381d4f253a42ddf48c46017ff8c6e1538a
manismk <manismkmech@gmail.com>
2022-10-17 21:11:00 +0530

    feat: added styles

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ export const App = `
       <h1 class="header">Counter App</h1>
       <div class="count" id="count"></div>
       <div class="btn--container">
-        <button class="btn" id="decrement">
+        <button class="btn btn--information" id="decrement">
           -
         </button>
-        <button class="btn" id="increment">
+        <button class="btn btn--information" id="increment">
           +
         </button>
       </div>
